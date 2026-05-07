c7dddfb13b89dbd24fb2f73c5fe0f3356e7c88ea
radar roark <122068506+xeubie@users.noreply.github.com>
2026-05-07 14:53:00 -0400

    add event.consume and store object id in db

--- src/event.zig
+++ src/event.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
+const xit = @import("xit");
+const rp = xit.repo;
+const hash = xit.hash;
 
-pub const id_size: usize = 32;
+const event_id_size: usize = 32;
 
 pub const EventData = union(enum) {
     issue: struct {
@@ -11,12 +14,124 @@ pub const EventData = union(enum) {
 };
 
 pub const Event = struct {
-    id: [id_size * 2]u8,
+    id: [event_id_size * 2]u8,
     data: EventData,
 };
 
-pub fn randomId(random: std.Random) [id_size]u8 {
-    var id_bytes: [id_size]u8 = undefined;
+/// associates an Event with a git object id.
+/// we save this oid in the database so we can reliably re-consume
+/// events if their oid changed. this can happen if the branch
+/// is rebased and force-pushed.
+pub fn RepoEvent(comptime hash_kind: hash.HashKind) type {
+    return struct {
+        oid: [hash.byteLen(hash_kind)]u8,
+        event: Event,
+    };
+}
+
+pub fn randomId(random: std.Random) [event_id_size]u8 {
+    var id_bytes: [event_id_size]u8 = undefined;
     random.bytes(&id_bytes);
     return id_bytes;
+}
+
+pub fn consume(
+    comptime DB: type,
+    comptime hash_kind: hash.HashKind,
+    io: std.Io,
+    db: *DB,
+    db_file: std.Io.File,
+    repo_events: []RepoEvent(hash_kind),
+) !void {
+    const Ctx = struct {
+        repo_events: []RepoEvent(hash_kind),
+
+        pub fn run(ctx: @This(), cursor: *DB.Cursor(.read_write)) !void {
+            const moment = try DB.HashMap(.read_write).init(cursor.*);
+
+            // the map with all of haxy's state and materialized views
+            const haxy_cursor = try moment.putCursor(hash.hashInt(hash_kind, "haxy"));
+            const haxy = try DB.HashMap(.read_write).init(haxy_cursor);
+
+            // try reading the last object id that was consumed
+            var last_object_id_maybe: ?[hash.byteLen(hash_kind)]u8 = null;
+            if (try haxy.getCursor(hash.hashInt(hash_kind, "last-object-id"))) |last_object_id_cursor| {
+                var last_object_id_buffer: [hash.byteLen(hash_kind)]u8 = undefined;
+                _ = try last_object_id_cursor.readBytes(&last_object_id_buffer);
+                last_object_id_maybe = last_object_id_buffer;
+            }
+
+            // for each event we want to consume...
+            for (ctx.repo_events) |repo_event| {
+                // map with the views as they appeared when each event was consumed.
+                // we can use this to see (and revert) the views to any previous state.
+                const object_id_to_views_cursor = try haxy.putCursor(hash.hashInt(hash_kind, "object-id->views"));
+                const object_id_to_views = try DB.HashMap(.read_write).init(object_id_to_views_cursor);
+
+                // create a new views map for the current event we are consuming
+                var views_cursor = try object_id_to_views.putCursor(hash.bytesToInt(hash_kind, &repo_event.oid));
+
+                // if there was a previous event, set the views map to have the same value as it.
+                // this efficiently "clones" the map so we make further modifications based on it.
+                if (last_object_id_maybe) |*last_object_id| {
+                    if (try object_id_to_views.getCursor(hash.bytesToInt(hash_kind, last_object_id))) |last_view_cursor| {
+                        try views_cursor.write(.{ .slot = last_view_cursor.slot() });
+                    }
+                }
+
+                // consume the event into the views map
+                {
+                    const event = repo_event.event;
+
+                    // get the id of the current event as bytes
+                    var current_event_id: [event_id_size]u8 = undefined;
+                    _ = try std.fmt.hexToBytes(&current_event_id, &event.id);
+
+                    const views = try DB.HashMap(.read_write).init(views_cursor);
+
+                    switch (event.data) {
+                        .issue => |data| {
+                            const event_id_to_issue_cursor = try views.putCursor(hash.hashInt(hash_kind, "event-id->issue"));
+                            const event_id_to_issue = try DB.HashMap(.read_write).init(event_id_to_issue_cursor);
+
+                            const issue_cursor = try event_id_to_issue.putCursor(hash.hashInt(hash_kind, &current_event_id));
+                            const issue = try DB.HashMap(.read_write).init(issue_cursor);
+
+                            try putIfDifferent(issue, hash.hashInt(hash_kind, "title"), data.title);
+                            try putIfDifferent(issue, hash.hashInt(hash_kind, "description"), data.description);
+                        },
+                    }
+                }
+
+                // the current object id is now the last one
+                last_object_id_maybe = repo_event.oid;
+
+                // prevent any of the data created above from being mutated by future iterations of this loop
+                try cursor.db.freeze();
+            }
+
+            if (last_object_id_maybe) |*last_object_id| {
+                try haxy.put(hash.hashInt(hash_kind, "last-object-id"), .{ .bytes = last_object_id });
+            }
+        }
+
+        fn putIfDifferent(map: DB.HashMap(.read_write), key: hash.HashInt(hash_kind), value: []const u8) !void {
+            if (try map.getCursor(key)) |value_cursor| {
+                var buffer: [4096]u8 = undefined;
+                const existing_value = try value_cursor.readBytes(&buffer);
+                if (std.mem.eql(u8, existing_value, value)) {
+                    return;
+                }
+            }
+
+            try map.put(key, .{ .bytes = value });
+        }
+    };
+
+    try db_file.lock(io, .exclusive);
+    defer db_file.unlock(io);
+
+    // create a new transaction in the database that runs the above-defined Ctx function
+    const history = try DB.ArrayList(.read_write).init(db.rootCursor());
+    try history.appendContext(.{ .slot = try history.getSlot(-1) }, Ctx{ .repo_events = repo_events });
 }
--- src/test/event.zig
+++ src/test/event.zig
@@ -40,7 +40,7 @@ test "simple" {
 
     const first_event_id = evt.randomId(prng.random());
 
-    const events_to_process = [_]evt.Event{
+    const events_to_consume = [_]evt.Event{
         .{
             .id = std.fmt.bytesToHex(first_event_id, .lower),
             .data = .{
@@ -95,7 +95,7 @@ test "simple" {
         var json: std.Io.Writer.Allocating = .init(std.testing.allocator);
         defer json.deinit();
 
-        for (events_to_process) |event| {
+        for (events_to_consume) |event| {
             json.clearRetainingCapacity();
 
             try std.json.Stringify.value(event, .{}, &json.writer);
@@ -109,12 +109,16 @@ test "simple" {
     // read and parse all of the events from the repo
     //
 
-    var events: std.ArrayList(evt.Event) = .empty;
+    var events: std.ArrayList(evt.RepoEvent(repo_opts.hash)) = .empty;
     defer events.deinit(allocator);
 
     {
-        var event_strs: std.ArrayList([]const u8) = .empty;
-        defer event_strs.deinit(allocator);
+        const Commit = struct {
+            oid: [hash.byteLen(repo_opts.hash)]u8,
+            message: []const u8,
+        };
+        var commits: std.ArrayList(Commit) = .empty;
+        defer commits.deinit(allocator);
 
         const ref_haxy_meta = try repo.readRef(io, .{ .kind = .head, .name = "haxy/meta" }) orelse return error.RefNotFound;
         var commit_iter = try repo.log(io, allocator, &.{ref_haxy_meta});
@@ -127,112 +131,31 @@ test "simple" {
             try commit_object.object_reader.seekTo(commit_object.content.commit.message_position);
             const message = try commit_object.object_reader.interface.allocRemaining(arena.allocator(), .unlimited);
 
-            try event_strs.append(allocator, message);
+            var commit: Commit = .{
+                .oid = undefined,
+                .message = message,
+            };
+            _ = try std.fmt.hexToBytes(&commit.oid, &commit_object.oid);
+
+            try commits.append(allocator, commit);
         }
 
         // parse commit messages as JSON into event values.
         // add events in reverse order so the earliest event is first.
-        for (0..event_strs.items.len) |i| {
-            const event_str = event_strs.items[event_strs.items.len - i - 1];
-            const event = try std.json.parseFromSliceLeaky(evt.Event, arena.allocator(), event_str, .{});
-            try events.append(allocator, event);
+        for (0..commits.items.len) |i| {
+            const commit = commits.items[commits.items.len - i - 1];
+            const event = try std.json.parseFromSliceLeaky(evt.Event, arena.allocator(), commit.message, .{});
+            try events.append(allocator, .{ .oid = commit.oid, .event = event });
         }
     }
 
     //
-    // process events into the database
+    // consume events into the database
     //
 
-    const Ctx = struct {
-        events: []evt.Event,
+    try evt.consume(Repo.DB, repo_opts.hash, io, &repo.core.db, repo.core.db_file, events.items);
 
-        pub fn run(ctx: @This(), cursor: *Repo.DB.Cursor(.read_write)) !void {
-            const moment = try Repo.DB.HashMap(.read_write).init(cursor.*);
-
-            // the map with all of haxy's state and materialized views
-            const haxy_cursor = try moment.putCursor(hash.hashInt(repo_opts.hash, "haxy"));
-            const haxy = try Repo.DB.HashMap(.read_write).init(haxy_cursor);
-
-            // try reading the last event id that was processed in the db
-            var last_event_id_maybe: ?[evt.id_size]u8 = null;
-            if (try haxy.getCursor(hash.hashInt(repo_opts.hash, "last-event-id"))) |last_event_id_cursor| {
-                var last_event_id_buffer: [evt.id_size]u8 = undefined;
-                _ = try last_event_id_cursor.readBytes(&last_event_id_buffer);
-                last_event_id_maybe = last_event_id_buffer;
-            }
-
-            // for each event we want to process...
-            for (ctx.events) |event| {
-                // get the id of the current event as bytes
-                var current_event_id_buffer: [evt.id_size]u8 = undefined;
-                _ = try std.fmt.hexToBytes(&current_event_id_buffer, &event.id);
-
-                // map with the views as they appeared when each event was processed.
-                // we can use this to see (and revert) the views to any previous state.
-                const event_id_to_views_cursor = try haxy.putCursor(hash.hashInt(repo_opts.hash, "event-id->views"));
-                const event_id_to_views = try Repo.DB.HashMap(.read_write).init(event_id_to_views_cursor);
-
-                // create a new views map for the current event we are processing
-                var views_cursor = try event_id_to_views.putCursor(hash.hashInt(repo_opts.hash, &current_event_id_buffer));
-
-                // if there was a previous event, set the views map to have the same value as it.
-                // this efficiently "clones" the map so we make further modifications based on it.
-                if (last_event_id_maybe) |*last_event_id| {
-                    if (try event_id_to_views.getCursor(hash.hashInt(repo_opts.hash, last_event_id))) |last_view_cursor| {
-                        try views_cursor.write(.{ .slot = last_view_cursor.slot() });
-                    }
-                }
-
-                // process the event into the views map
-                {
-                    const views = try Repo.DB.HashMap(.read_write).init(views_cursor);
-
-                    switch (event.data) {
-                        .issue => |data| {
-                            const event_id_to_issue_cursor = try views.putCursor(hash.hashInt(repo_opts.hash, "event-id->issue"));
-                            const event_id_to_issue = try Repo.DB.HashMap(.read_write).init(event_id_to_issue_cursor);
-
-                            const issue_cursor = try event_id_to_issue.putCursor(hash.hashInt(repo_opts.hash, &current_event_id_buffer));
-                            const issue = try Repo.DB.HashMap(.read_write).init(issue_cursor);
-
-                            try putIfDifferent(issue, hash.hashInt(repo_opts.hash, "id"), &current_event_id_buffer);
-                            try putIfDifferent(issue, hash.hashInt(repo_opts.hash, "title"), data.title);
-                            try putIfDifferent(issue, hash.hashInt(repo_opts.hash, "description"), data.description);
-                        },
-                    }
-                }
-
-                // the current event id is now the last one
-                last_event_id_maybe = current_event_id_buffer;
-
-                // prevent any of the data created above from being mutated by future iterations of this loop
-                try cursor.db.freeze();
-            }
-
-            if (last_event_id_maybe) |*last_event_id| {
-                try haxy.put(hash.hashInt(repo_opts.hash, "last-event-id"), .{ .bytes = last_event_id });
-            }
-        }
-
-        fn putIfDifferent(map: Repo.DB.HashMap(.read_write), key: hash.HashInt(repo_opts.hash), value: []const u8) !void {
-            if (try map.getCursor(key)) |value_cursor| {
-                var buffer: [4096]u8 = undefined;
-                const existing_value = try value_cursor.readBytes(&buffer);
-                if (std.mem.eql(u8, existing_value, value)) {
-                    return;
-                }
-            }
-
-            try map.put(key, .{ .bytes = value });
-        }
-    };
-
-    try repo.core.db_file.lock(io, .exclusive);
-    defer repo.core.db_file.unlock(io);
-
-    // create a new transaction in the database that runs the above-defined Ctx function
-    const history = try Repo.DB.ArrayList(.read_write).init(repo.core.db.rootCursor());
-    try history.appendContext(.{ .slot = try history.getSlot(-1) }, Ctx{ .events = events.items });
+    const history = try Repo.DB.ArrayList(.read_only).init(repo.core.db.rootCursor().readOnly());
 
     // read the moment we just created
     const moment_cursor = try history.getCursor(-1) orelse return error.NotFound;
@@ -241,18 +164,18 @@ test "simple" {
     const haxy_cursor = try moment.getCursor(hash.hashInt(repo_opts.hash, "haxy")) orelse return error.NotFound;
     const haxy = try Repo.DB.HashMap(.read_only).init(haxy_cursor);
 
-    const event_id_to_views_cursor = try haxy.getCursor(hash.hashInt(repo_opts.hash, "event-id->views")) orelse return error.NotFound;
-    const event_id_to_views = try Repo.DB.HashMap(.read_only).init(event_id_to_views_cursor);
+    const object_id_to_views_cursor = try haxy.getCursor(hash.hashInt(repo_opts.hash, "object-id->views")) orelse return error.NotFound;
+    const object_id_to_views = try Repo.DB.HashMap(.read_only).init(object_id_to_views_cursor);
 
     // make sure the issue from the first event was correctly edited
     {
-        // get the last event id
-        const last_event_id_cursor = try haxy.getCursor(hash.hashInt(repo_opts.hash, "last-event-id")) orelse return error.NotFound;
-        var last_event_id_buffer: [evt.id_size]u8 = undefined;
-        _ = try last_event_id_cursor.readBytes(&last_event_id_buffer);
+        // get the last object id
+        const last_object_id_cursor = try haxy.getCursor(hash.hashInt(repo_opts.hash, "last-object-id")) orelse return error.NotFound;
+        var last_object_id: [hash.byteLen(repo_opts.hash)]u8 = undefined;
+        _ = try last_object_id_cursor.readBytes(&last_object_id);
 
-        // get the latest views (the views generated by the last event id)
-        const views_cursor = try event_id_to_views.getCursor(hash.hashInt(repo_opts.hash, &last_event_id_buffer)) orelse return error.NotFound;
+        // get the latest views (the views generated by the last object id)
+        const views_cursor = try object_id_to_views.getCursor(hash.bytesToInt(repo_opts.hash, &last_object_id)) orelse return error.NotFound;
         const views = try Repo.DB.HashMap(.read_only).init(views_cursor);
 
         // get the map of issues
@@ -267,6 +190,6 @@ test "simple" {
         const first_issue_description_cursor = try first_issue.getCursor(hash.hashInt(repo_opts.hash, "description")) orelse return error.NotFound;
         const first_issue_description_value = try first_issue_description_cursor.readBytesAlloc(allocator, null);
         defer allocator.free(first_issue_description_value);
-        try std.testing.expectEqualStrings(events_to_process[1].data.issue.description, first_issue_description_value);
+        try std.testing.expectEqualStrings(events_to_consume[1].data.issue.description, first_issue_description_value);
     }
 }
